0996cb7c26cb65d4f16586095ff1074f2edeeb0b
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2025-12-19 00:52:30 +0200

    Implemented chunking

--- src/gosort.go
+++ src/gosort.go
@@ -2,60 +2,56 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand/v2"
 	"os"
+	"slices"
 	"strconv"
-	"time"
 )
 
-func isInt(input string) bool {
-	value := false
-	n, err := strconv.Atoi(input)
-
-	if err == nil {
-		value = true
-	}
-
-	if n < 0 {
-		value = false
-	}
-	return value
-	// using strconv.Atoi() to convert string to int
-	// converted int goes in 'n' and err is set to nil
-	// if conversion fails, then n gets default value 0 and error object (info) goes into err
-}
-
 func main() {
-	// Range of values for random integer generation
+
+	// range of values for random integer generation
 	const MAX int = 1000
 	const MIN int = 1
 
-	// Check to see if user enters less than 3 args in terminal
+	// check # of arguments entered
 	if len(os.Args) < 3 {
-		fmt.Println("Usage: \n-r <number> \n-i <file path to .txt> \n-d <incoming directory of .txt files>")
+		fmt.Println("Usage:")
+		fmt.Println("-r <number>")
+		fmt.Println("-i <file path to .txt>")
+		fmt.Println("-d <incoming directory of .txt files>")
 		return
 	}
-	
-	// Check to see if the os.Args is valid integer greater than 0
+
+	// convert input argument to an integer
 	n, err := strconv.Atoi(os.Args[2])
-		if err != nil || n == 0 {
-			fmt.Println("Error: argument must be a positive integer greater than 0")
-			return
-		}
-
-		rand.Seed(time.Now().UnixNano())
-		nums := make([]int, n)
-
-		for i:=0, i<n, i++ {
-			nums[i] = rand.Intn(MAX-MIN) + MIN
-		}
+	if err != nil || n < 10 {
+		fmt.Println("Error: argument must be a positive integer at least 10")
+		return
 	}
-	
 
+	// create a slice of 'n' number of ints
+	nums := make([]int, n)
 
+	for i := 0; i < n; i++ {
+		nums[i] = rand.IntN(MAX-MIN+1) + MIN
+		// test print to see if it works
+		fmt.Printf("Value %d: %d\n", i, nums[i])
+	}
 
-	//fmt.Println(n)
-	//fmt.Println("input was" + os.Args[2])
-	//fmt.Println(os.Args[1])
+	// chunking
+	numOfChunks := int(math.Ceil(math.Sqrt(float64(n))))
+	if numOfChunks < 4 {
+		numOfChunks = 4
+	}
+	// create chunks of size n from the nums array
+	// slices.Chunk(slice, size)l
+	var size int = 2
+	chunks := slices.Chunk(nums, size)
 
 }
+
+//fmt.Println(n)
+//fmt.Println("input was" + os.Args[2])
+//fmt.Println(os.Args[1])
